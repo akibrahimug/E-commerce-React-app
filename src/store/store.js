5074d8dcea271e38107b9610dbb6b0b2a7f59ce4
kasoma <kasomaibrahim@gmail.com>
2024-03-05 23:34:27 +0000

    adding redux-persist to add state to the local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
-
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// configure persist state in our local storgae using redux-persist
+const persistConfig = {
+  key: "root",
+  storage,
+  // state i dont want to add to local
+  blacklist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+export const persistor = persistStore(store);
